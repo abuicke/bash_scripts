25d2ad34fd8c91345c2ea1f934c13113f4040a96
Adam Buicke <buickea@gmail.com>
2022-03-27 08:51:09 +0100

    added super user check to update script

--- update.sh
+++ update.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+
+# Check that the script is being
+# run with super user privileges
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
 dnf clean all
 dnf autoremove
 dnf update -y
