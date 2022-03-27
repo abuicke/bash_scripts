40feb1a177dca507aa2a827961e8023b0ffc3d97
Adam Buicke <buickea@gmail.com>
2022-03-27 08:43:21 +0100

    added root check to windows partition mount script

--- mount_windows_partition.sh
+++ mount_windows_partition.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# Check that the script is being
+# run with super user privileges
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
 if [ $1 = "mount" ]; then
 	mount -t ntfs-3g /dev/sda3 /mnt/nts
 	echo "Windows partition now mounted at /mnt/nts"
