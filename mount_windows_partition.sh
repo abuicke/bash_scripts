461cda355a187c6a2f4bf5b59af79cb325196a52
Adam Buicke <buickea@gmail.com>
2022-03-27 09:24:58 +0100

    fixed argument error checking in windows mount script

--- mount_windows_partition.sh
+++ mount_windows_partition.sh
@@ -7,10 +7,12 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-if [ $1 = "mount" ]; then
+if [ "$#" -ne 1 ]; then
+	echo "script takes 1 argument, use \"mount\" or \"unmount\" option after script name"
+elif [ $1 = "mount" ]; then
 	mount -t ntfs-3g /dev/sda3 /mnt/nts
 	echo "Windows partition now mounted at /mnt/nts"
- elif [ $1 = "unmount" ]; then
+elif [ $1 = "unmount" ]; then
 	umount /mnt/nts
 	echo "Windows partition unmounted"
 else
